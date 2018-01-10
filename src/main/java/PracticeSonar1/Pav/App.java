5bc900d39b00d8a1ea7d2065b2db86e2bee584a4
pavanisura <pavani.sura267@gmail.com>
2018-01-10 10:21:17 +0530

    this is my first commit

---
+++ src/main/java/PracticeSonar1/Pav/App.java
@@ -0,0 +1,13 @@
+package PracticeSonar1.Pav;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
